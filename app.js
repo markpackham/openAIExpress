b678dd8a73a840fc4e2c50c3f4939be2861da59c
mark <markpackham1@gmail.com>
2023-06-06 18:13:04 +0100

    use Express
    https://youtu.be/ynyohARvZD4?t=162

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-const readline = require("readline");
+// const readline = require("readline");
+const express = require("express");
 const { generateMeta } = require("./controllers/openaiController");
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
 
 // rl.question("YouTube Video Title: \n", (title) => console.log(title));
-rl.question("YouTube Video Title: \n", generateMeta);
+// rl.question("YouTube Video Title: \n", generateMeta);
+
+const app = express();
+app.listen(4000, () => console.log("Listening to requests on port 4000"));
