9265d6c41740c3406f8983c7976f05a14ec62071
mark <markpackham1@gmail.com>
2023-06-06 18:26:02 +0100

    implement middleware
    https://youtu.be/ynyohARvZD4?t=180

--- app.js
+++ app.js
@@ -12,3 +12,5 @@ const { generateMeta } = require("./controllers/openaiController");
 
 const app = express();
 app.listen(4000, () => console.log("Listening to requests on port 4000"));
+
+app.use(express.json());
