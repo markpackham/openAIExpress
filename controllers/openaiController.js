52e265f408585dcc8b81295fb1ffd3209f48d94e
mark <markpackham1@gmail.com>
2023-06-06 17:17:07 +0100

    add generateMeta info function
    https://youtu.be/dV60qHIX2Qg?t=511

---
+++ controllers/openaiController.js
@@ -0,0 +1,14 @@
+const openai = require("../config/openaiConfig");
+
+const generateMeta = async (title) => {
+  const description = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "user",
+        content: `Come up with a description for a YouTube vidoe called ${title}`,
+      },
+    ],
+    max_tokens: 100,
+  });
+};
