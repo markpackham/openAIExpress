994eaff7ac400e4a65a7f290daf191925b27a33e
mark <markpackham1@gmail.com>
2023-06-07 10:35:53 +0100

    add front end styling
    https://youtu.be/2RXu31Clpmk?t=421

--- app.js
+++ app.js
@@ -16,7 +16,10 @@ const {
 const app = express();
 app.listen(4000, () => console.log("Listening to requests on port 4000"));
 
+// middleware
 app.use(express.json());
+app.use(express.static("public"));
 
+// routes
 app.post("/openai/meta", generateMeta);
 app.post("/openai/image", generateImage);
